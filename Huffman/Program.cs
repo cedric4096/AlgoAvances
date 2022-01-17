e096611c3d9dd0addb48154541bf6d36128ab292
Cedric Klodzinski <cedric99@sfr.fr>
2022-01-17 10:30:24 +0100

    Comments

--- Huffman/Program.cs
+++ Huffman/Program.cs
@@ -1,5 +1,8 @@
 ﻿using Huffman;
 
+/// <summary>
+/// Prints the binary code of the specified Huffman tree in the specified string.
+/// </summary>
 static void printHuffman(HuffmanNode root, string str)
 {
 	if (root.LeftNode == null && root.RightNode == null && root.Character != null)
@@ -15,13 +18,16 @@ static void printHuffman(HuffmanNode root, string str)
 Console.WriteLine("Enter text to compress :");
 string? toEnc = Console.ReadLine();
 
+// Ensuring the entered string is not null
 if (toEnc is null)
 {
 	throw new Exception("Null string");
 }
 
+// List of character frequencies
 List<(char, int)> frequencies = new List<(char, int)>();
 
+// For each character in entered string, count occurences and add to frequencies
 foreach (char character in toEnc)
 {
 	int found = frequencies.FindIndex(pair => pair.Item1.Equals(character));
@@ -35,8 +41,10 @@ foreach (char character in toEnc)
 	}
 }
 
+// Character priority queue
 PriorityQueue<HuffmanNode, int> priorities = new PriorityQueue<HuffmanNode, int>();
 
+// For each frequency, create a new node in Huffman tree
 foreach ((char, int) value in frequencies)
 {
 	HuffmanNode node = new HuffmanNode() { Character = value.Item1, Frequency = value.Item2, LeftNode = null, RightNode = null };
@@ -44,8 +52,10 @@ foreach ((char, int) value in frequencies)
 	priorities.Enqueue(node, value.Item2);
 }
 
+// Root of Huffman tree
 HuffmanNode? root = null;
 
+// While there are nodes in priorities, dequeue 2 nodes and store them as new node children (new root), then enqueue this new node
 while (priorities.Count > 1)
 {
 	HuffmanNode left = priorities.Dequeue();
